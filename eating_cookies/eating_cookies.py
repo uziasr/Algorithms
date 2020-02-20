ef33712e08ed2e9245bc5cec6e2bb5f4237c3f29
Uzias Rivera <uziasrivera@gmail.com>
2020-02-20 12:11:23 -0800

    completed some algorithms

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,8 +6,52 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+    if n == 0:
+      return 1
+    elif n<0:
+      return 0
+    elif cache and cache[n] > 0:
+      return cache[n]
+    else:
+      if cache is None:
+        cache = {}
+        # print(cache)
+      value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+      cache[n] = value
+      return value
+    
+print(eating_cookies(900, {})/1000000000000)
 
+
+# else:
+#       if cache is None:
+#         cache = { i:0 for i in range(n+1)}
+#         print(cache)
+#       value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+#       cache[n] = value
+#       return value
+    
+#1 (1,1,1,1,1) 
+#2 (1,1,1,2)
+#3 (1,1,2,1)
+#4 (1,2,1,1)
+#5 (1,2,2)
+#6 (1,1,3)
+#7 (1,3,1)
+#
+#8 (2,1,1,1)
+#9 (2,1,2)
+#10 (2,2,1)
+#11 (2,3)
+# 
+#12 (3,1,1)
+#13 (3,2)
+# 
+# 
+# 
+# 
+#  
+  
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
